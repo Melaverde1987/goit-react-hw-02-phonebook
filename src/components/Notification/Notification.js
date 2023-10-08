e8194c82823735b548d3cb9faf20376372df280b
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-10-09 00:22:08 +0200

    first commit

--- src/components/Notification/Notification.js
+++
@@ -1,9 +0,0 @@
-import css from './Notification.module.css';
-
-export const Notification = ({ message }) => {
-  return (
-    <div className={css.container}>
-      <p className={css.text}>{message}</p>
-    </div>
-  );
-};
--- src/components/Section/Section.js
+++
@@ -1,8 +0,0 @@
-import css from './Section.module.css';
-
-export const Section = ({ title, children }) => (
-  <section className={css.card}>
-    <h2>{title}</h2>
-    {children}
-  </section>
-);
