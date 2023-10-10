8a1fde8c176b36946147459bfa2e830bd154206b
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-10-11 01:51:42 +0200

    styling5

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,26 +2,24 @@ import { ContactListItem } from '../ContactListItem/ContactListItem';
 import { List, ListItem } from './ContactList.styled';
 
 export const ContactList = ({ contacts, onDelete }) => {
-  if (contacts !== '') {
-    return (
-      <List>
-        {contacts.map(contact => (
-          <ListItem key={contact.id}>
-            <ContactListItem
-              contactName={contact.name}
-              contactNumber={contact.number}
-            ></ContactListItem>
-            <button
-              type="button"
-              id={contact.name}
-              className="btn btn-outline"
-              onClick={onDelete}
-            >
-              Delete
-            </button>
-          </ListItem>
-        ))}
-      </List>
-    );
-  }
+  return (
+    <List>
+      {contacts.map(contact => (
+        <ListItem key={contact.id}>
+          <ContactListItem
+            contactName={contact.name}
+            contactNumber={contact.number}
+          ></ContactListItem>
+          <button
+            type="button"
+            id={contact.name}
+            className="btn btn-outline"
+            onClick={onDelete}
+          >
+            Delete
+          </button>
+        </ListItem>
+      ))}
+    </List>
+  );
 };
